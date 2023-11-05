49abd5af43649861a72e62f3ec43d9ea798d7c17
dandriyanov <sunny.flag1818@fastmail.com>
2023-11-05 13:26:39 +0400

    Initialize the .git directory

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
-#[allow(unused_imports)]
-use std::env;
-#[allow(unused_imports)]
+use anyhow::Result;
+use clap::{Parser, Subcommand};
 use std::fs;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+#[derive(Parser, Debug)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
 
-    // Uncomment this block to pass the first stage
-    // let args: Vec<String> = env::args().collect();
-    // if args[1] == "init" {
-    //     fs::create_dir(".git").unwrap();
-    //     fs::create_dir(".git/objects").unwrap();
-    //     fs::create_dir(".git/refs").unwrap();
-    //     fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
-    //     println!("Initialized git directory")
-    // } else {
-    //     println!("unknown command: {}", args[1])
-    // }
+#[derive(Subcommand, Debug)]
+enum Command {
+    Init,
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+    match cli.command {
+        Command::Init => {
+            fs::create_dir(".git")?;
+            fs::create_dir(".git/objects")?;
+            fs::create_dir(".git/refs")?;
+            fs::write(".git/HEAD", "ref: refs/heads/master\n")?;
+            println!("Initialized git directory")
+        }
+    }
+    Ok(())
 }
