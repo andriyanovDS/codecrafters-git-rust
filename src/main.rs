900d3eff6b36e98f960846c82b65c1586d823bf6
dandriyanov <sunny.flag1818@fastmail.com>
2023-11-05 15:45:23 +0400

    Read a blob object

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use clap::{Parser, Subcommand};
-use std::fs;
+use std::{fs, io::Read};
 
 #[derive(Parser, Debug)]
 struct Cli {
@@ -9,8 +9,14 @@ struct Cli {
 }
 
 #[derive(Subcommand, Debug)]
+#[clap(rename_all = "kebab-case")]
 enum Command {
     Init,
+    CatFile {
+        #[clap(short)]
+        print: bool,
+        hash: String,
+    },
 }
 
 fn main() -> Result<()> {
@@ -22,6 +28,16 @@ fn main() -> Result<()> {
             fs::create_dir(".git/refs")?;
             fs::write(".git/HEAD", "ref: refs/heads/master\n")?;
             println!("Initialized git directory")
+        }
+        Command::CatFile { print: _, hash } => {
+            let directory = &hash[0..2];
+            let file_name = &hash[2..];
+            let file_path = format!(".git/objects/{}/{}", directory, file_name);
+            let file_content = fs::read(&file_path)?;
+            let mut decoder = flate2::read::ZlibDecoder::new(file_content.as_slice());
+            let mut result = String::new();
+            decoder.read_to_string(&mut result)?;
+            print!("{}", result);
         }
     }
     Ok(())
